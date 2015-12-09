92d376e2debb13ac45d2d4bdde5a88a50b40caf8
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-12-09 04:01:56 +0100

    Ceph image extended to support mounting arbitrary directories/disks (including journal file) for Ceph OSD - all of them will be handled and picked up nicely, OSDs registered if necessary and so on

--- ceph/webserver-entrypoint.sh
+++ ceph/webserver-entrypoint.sh
@@ -21,26 +21,49 @@ if [ ! "$CEPH_PUBLIC_NETWORK" ]; then
 	export CEPH_PUBLIC_NETWORK=`ip addr | grep $MON_IP | awk '{ print $2 }'`
 fi
 
-if [[ "$1" == 'osd' && "$OSD_TYPE" == 'directory' && "`find /var/lib/ceph/osd -prune -empty`" ]]; then
-	# From original entrypoint to get all configs from Consul
-	source /config.kv.sh
-	get_config
-	if [[ ! -e /etc/ceph/${CLUSTER}.conf ]]; then
-	  echo "ERROR- /etc/ceph/${CLUSTER}.conf must exist; get it from your existing mon"
-	  exit 1
-	fi
-	if [ ${CEPH_GET_ADMIN_KEY} -eq "1" ]; then
-		get_admin_key
-		if [[ ! -e /etc/ceph/${CLUSTER}.client.admin.keyring ]]; then
-			echo "ERROR- /etc/ceph/${CLUSTER}.client.admin.keyring must exist; get it from your existing mon"
-			exit 1
+if [[ "$1" == 'osd' && "$OSD_TYPE" == 'directory' ]]; then
+	if [ "`find /var/lib/ceph/osd -prune -empty`" ]; then
+		# From original entrypoint to get all configs from Consul
+		source /config.kv.sh
+		get_config
+		if [[ ! -e /etc/ceph/${CLUSTER}.conf ]]; then
+		  echo "ERROR- /etc/ceph/${CLUSTER}.conf must exist; get it from your existing mon"
+		  exit 1
+		fi
+		if [ ${CEPH_GET_ADMIN_KEY} -eq "1" ]; then
+			get_admin_key
+			if [[ ! -e /etc/ceph/${CLUSTER}.client.admin.keyring ]]; then
+				echo "ERROR- /etc/ceph/${CLUSTER}.client.admin.keyring must exist; get it from your existing mon"
+				exit 1
+			fi
+		fi
+
+		OSD_ID=`ceph --cluster $CLUSTER osd create`
+		mkdir /var/lib/ceph/osd/$CLUSTER-$OSD_ID
+		chown -R ceph: /var/lib/ceph/osd/$CLUSTER-$OSD_ID
+	else
+		DIRECTORY=`ls -d /var/lib/ceph/osd/* | awk '{ print $1; exit }'`
+		OSD_ID=`echo $DIRECTORY | tr '-' ' ' | awk '{ print $2; exit }'`
+		# If directory was mounted, but not initialized yet - lets do it
+		if [ ! -e $DIRECTORY/fsid ]; then
+			chown -R ceph: $DIRECTORY
+			# Directory might be named in any way, so lets add symlink with normalized path
+			if [ "$OSD_ID" ]; then
+				# If OSD id was specified, but is known yet, so lets create OSD with specified id
+				if [[ ! `ceph --cluster $CLUSTER osd find $OSD_ID` ]]; then
+					ceph --cluster $CLUSTER osd create `uuidgen` $OSD_ID
+				fi
+			else
+				OSD_ID=`ceph --cluster $CLUSTER osd create`
+				ln -s $DIRECTORY /var/lib/ceph/osd/$CLUSTER-$OSD_ID
+			fi
+		else
+			# Even if everything is initialized, directory might be named in any way, so lets add symlink with normalized path
+			if [ ! "$OSD_ID" ]; then
+				ln -s $DIRECTORY /var/lib/ceph/osd/$CLUSTER-`cat $DIRECTORY/whoami`
+			fi
 		fi
 	fi
-
-	OSD_ID=`ceph --cluster $CLUSTER osd create`
-	mkdir /var/lib/ceph/osd/$CLUSTER-$OSD_ID
 fi
-
-chown -R ceph: /var/lib/ceph/osd
 
 exec /entrypoint.sh $@
