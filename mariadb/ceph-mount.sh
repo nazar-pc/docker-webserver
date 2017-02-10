eb66b3d5719a56ad0745c305558af56e229e721c
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-02-10 06:20:05 +0200

    Tweaking cephfs mounting, works with privileged containers for now. Fix nginx image to tag `1`.

--- mariadb/ceph-mount.sh
+++ mariadb/ceph-mount.sh
@@ -7,8 +7,10 @@ fi
 echo "Have some mount points to mount"
 
 mkdir -p /ceph
+mkdir -p /etc/ceph
+echo -e "[global]\nauth client required = none" > /etc/ceph/ceph.conf
 echo "Mounting /ceph..."
-while ! ceph-fuse -m $CEPH_MON_SERVICE /ceph; do
+while ! `ceph-fuse -m $CEPH_MON_SERVICE /ceph && mount | grep -q /ceph`; do
 	echo "Mounting failed, trying again in 1 second"
 	sleep 1
 done
