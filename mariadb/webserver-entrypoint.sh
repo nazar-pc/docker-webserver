16cc7288130e9840317797e9a1c7f1d2a727a5c6
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-02-17 03:45:46 +0200

    MariaDB now stores all its data in `/data/mysql` and upgrades from older configuration on first start. MariaDB fix for starting with v1 of `docker-compose.yml` file spec.

--- mariadb/webserver-entrypoint.sh
+++ mariadb/webserver-entrypoint.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-/webserver-common/ceph-mount.sh \
-	/data \
-	/etc/mysql \
-	/var/lib/mysql
+/webserver-common/ceph-mount.sh /data
 
-if [ ! -e /etc/mysql/my.cnf ]; then
-	cp -a /etc/mysql_dist/* /etc/mysql/
+# Automatic upgrade from older images
+# TODO: remove in future
+if [ -L /data/mysql/config ]; then
+	rm /data/mysql/config /data/mysql/data /data/mysql/log
+	mkdir /data/mysql/config /data/mysql/data /data/mysql/log
+	mv /etc/mysql/* /data/mysql/config/
+	mv /var/lib/mysql/* /data/mysql/data/
+	mv /var/log/mysql/* /data/mysql/log/
+	sed -i 's/\/etc\/mysql/\/data\/mysql\/config/g' /data/mysql/config/my.cnf
+	sed -i 's/\/var\/lib\/mysql_local/\/data\/mysql\/data/g' /data/mysql/config/my.cnf
+	sed -i 's/\/var\/log\/mysql/\/data\/mysql\/log/g' /etc/mysql/my.cnf
 fi
 
 if [ ! -e /data/mysql ]; then
-	mkdir -p /data/mysql
-	ln -s /etc/mysql /data/mysql/config
-	ln -s /var/log/mysql /data/mysql/log
-	ln -s /var/lib/mysql /data/mysql/data
+	mkdir -p /data/mysql/config /data/mysql/data /data/mysql/log
+	cp -a /etc/mysql_dist/* /data/mysql/config/
 fi
 
-chown git:git /data
-chown -R git:git /data/mysql /etc/mysql
-chown mysql:mysql /var/lib/mysql /var/log/mysql /var/run/mysqld
-chmod 770 /var/lib/mysql
+chown git:git /data /data/mysql
+chown -R git:git /data/mysql/config
+chown mysql:mysql /data/mysql/data /data/mysql/log
+chmod 770 /data/mysql/data
 
 if [ ! -e /data/mysql/root_password ]; then
 	pwgen -s 30 1 > /data/mysql/root_password
@@ -28,34 +32,40 @@ fi
 export MYSQL_ROOT_PASSWORD=`cat /data/mysql/root_password`
 echo "MySQL root password (from /data/mysql/root_password): $MYSQL_ROOT_PASSWORD"
 
-# Initialize MariaDB using entrypoint from original image without last line
-params_before="$@ --wsrep_sst_method=xtrabackup-v2 --wsrep_sst_auth=root:$MYSQL_ROOT_PASSWORD"
-set -- $@ --bind_address=127.0.0.1 --wsrep_cluster_address=gcomm:// --wsrep_on=OFF
-gosu mysql bash /docker-entrypoint-init.sh "$@"
+if [ "$1" = 'mysqld' ]; then
+	shift
+fi
+set -- --wsrep_sst_method=xtrabackup-v2 --wsrep_sst_auth=root:$MYSQL_ROOT_PASSWORD $@
 
-# If this is not the master node of the service (first instance that have started) - do not use /var/lib/mysql and try to connect to the master node
+# If this is not the master node of the service (first instance that have started) - do not use /data/mysql/data and /data/mysql/log and try to connect to the master node
 /webserver-common/determine-service-master-node.sh /data/mysql/master_node_ip $SERVICE_NAME
 master_node=`cat /data/mysql/master_node_ip`
-if [ ! "`cat /etc/hosts | grep $master_node`" ]; then
+if [[ "$master_node" &&  (! "`cat /etc/hosts | grep $master_node`") ]]; then
 	echo "Starting as regular node (no synchronization to permanent storage)"
-	if [ -L /var/lib/mysql_local ]; then
-		# Change link to local directory to avoid unavoidable conflicts with master node
-		rm /var/lib/mysql_local
-		mkdir /tmp/mysql_local
-		chown mysql:mysql /tmp/mysql_local
-		ln -s /tmp/mysql_local /var/lib/mysql_local
+	set -- --defaults-file=/tmp/mysql/config/my.cnf $@
+
+	if [ ! -e /tmp/mysql ]; then
+		mkdir -p /tmp/mysql/config /tmp/mysql/data /tmp/mysql/log
+		chown mysql:mysql /tmp/mysql/data /tmp/mysql/log
+		cp /data/mysql/config/my.cnf /tmp/mysql/config/my.cnf
+		sed -i 's/\/data\/mysql\/data/\/tmp\/mysql\/data/g' /tmp/mysql/config/my.cnf
+		sed -i 's/\/data\/mysql\/log/\/tmp\/mysql\/log/g' /tmp/mysql/config/my.cnf
 		# Initialize MariaDB using entrypoint from original image without last line
-		set -- $@ --bind_address=127.0.0.1 --wsrep_cluster_address=gcomm:// --wsrep_on=OFF
-		gosu mysql bash /docker-entrypoint-init.sh "%@"
+		gosu mysql /docker-entrypoint-init.sh "$@ --bind_address=127.0.0.1 --wsrep_cluster_address=gcomm:// --wsrep_on=OFF"
 	fi
 	if ! mysqladmin --host=$master_node --user=root --password=$MYSQL_ROOT_PASSWORD ping; then
 		echo 'Master node is not ready, exiting'
 		sleep 1
 	fi
-	params_before="$params_before --wsrep_cluster_address=gcomm://$master_node"
+	set -- $@ --wsrep_cluster_address=gcomm://$master_node
 else
 	echo "Starting as master node (with synchronization to permanent storage)"
 	hostname > /data/mysql/master_node_hostname
+	set -- --defaults-file=/data/mysql/config/my.cnf $@
+
+	# Initialize MariaDB using entrypoint from original image without last line
+	gosu mysql /docker-entrypoint-init.sh "$@ --bind_address=127.0.0.1 --wsrep_cluster_address=gcomm:// --wsrep_on=OFF"
+
 	# Find other existing node to connect to
 	existing_nodes=''
 	for node_ip in `/webserver-common/list-service-nodes.sh $SERVICE_NAME`; do
@@ -72,12 +82,12 @@ else
 	# but for now we assume master not to be always up to date
 	if [ ! "$existing_nodes" ]; then
 		echo "No existing alive nodes found in cluster, forcing start in master node"
-		sed -i 's/safe_to_bootstrap: 0/safe_to_bootstrap: 1/g' /var/lib/mysql_local/grastate.dat
+		if [ -e /data/mysql/data/grastate.dat ]; then
+			sed -i 's/safe_to_bootstrap: 0/safe_to_bootstrap: 1/g' /data/mysql/data/grastate.dat
+		fi
 	fi
-	params_before="$params_before --wsrep_cluster_address=gcomm://${existing_nodes:1}"
+	set -- $@ --wsrep_cluster_address=gcomm://${existing_nodes:1}
 fi
-
-set -- $params_before
 
 if [ -e /data/mysql/before_start.sh ]; then
 	bash /data/mysql/before_start.sh
@@ -85,4 +95,4 @@ else
 	touch /data/mysql/before_start.sh
 fi
 
-exec gosu mysql "$@"
+exec gosu mysql mysqld "$@"
