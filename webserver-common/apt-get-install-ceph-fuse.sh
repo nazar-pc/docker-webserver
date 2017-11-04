7c123a9b0acbf034d32f69465cbd7fd01381ea09
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-11-05 01:12:25 +0200

    Do not install Ceph until http://tracker.ceph.com/issues/21585 is resolved

--- webserver-common/apt-get-install-ceph-fuse.sh
+++ webserver-common/apt-get-install-ceph-fuse.sh
@@ -2,8 +2,9 @@
 
 # Ceph repository for up to date version of ceph-fuse package; Ceph-fuse itself is used as cluster filesystem CephFS
 
-CEPH_VERSION=kraken
-curl -sSL 'https://download.ceph.com/keys/release.asc' | apt-key add -
-echo "deb http://download.ceph.com/debian-$CEPH_VERSION/ $(cat /etc/apt/sources.list | awk '{ print $3; exit }') main" > /etc/apt/sources.list.d/ceph-$CEPH_VERSION.list
-apt-get update
-apt-get install -y --no-install-recommends ceph-fuse
+# TODO: Do not install Ceph until http://tracker.ceph.com/issues/21585 is resolved
+#CEPH_VERSION=kraken
+#curl -sSL 'https://download.ceph.com/keys/release.asc' | apt-key add -
+#echo "deb http://download.ceph.com/debian-$CEPH_VERSION/ $(cat /etc/apt/sources.list | awk '{ print $3; exit }') main" > /etc/apt/sources.list.d/ceph-$CEPH_VERSION.list
+#apt-get update
+#apt-get install -y --no-install-recommends ceph-fuse
